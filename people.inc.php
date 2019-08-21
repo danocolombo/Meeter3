57e8d2adb5de466dc9360504735ab08d3852a760
danocolombo@gmail.com <GR0mans1212!>
2019-08-21 06:45:34 -0400

    added for use in 3.0

---
+++ people.inc.php
@@ -0,0 +1,10 @@
+<?php
+    // this file is for use with the meeter people functionality.
+    
+    function people_select_all_commits(){
+        $enabledFields = getDisplayValueArray();
+        
+        foreach($enabledFields as $field){
+            echo "field: $field<br>";
+        }
+    }
