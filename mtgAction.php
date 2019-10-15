9cca60bad712bb44e0a9212dd8f8322c67f222f6
danocolombo@gmail.com <GR0mans1212!>
2019-10-15 01:28:09 -0400

    working on integrating add new meeting via API gateway

--- mtgAction.php
+++ mtgAction.php
@@ -13,7 +13,6 @@ date_default_timezone_set('UTC');
 use Aws\DynamoDb\Exception\DynamoDbException;
 
 include 'mtgRedirects.php';
-//include 'meeting.php';
 // require 'meeter.php';
 // require 'includes/database.inc.php';
 //require 'includes/meeting.inc.php';
@@ -58,89 +57,47 @@ function addMeetingToDB(){
     //     OR die(mysql_error());
     
     //we are going to check our values:
+    $daMtg = new meeting($_POST['mtgDate'], $_POST['rdoMtgType'], $_POST['mtgTitle']);
     
+    if(isset($_POST['mtgCoordinator'])){$daMtg->setMtgHost($_POST['mtgCoordinator']);}
+    if(isset($_POST['mtgAttendance'])){$daMtg->setMtgAttendance($_POST['mtgAttendance']);}
+    if(isset($_POST['mtgDonations'])){$daMtg->setDonations($_POST['mtgDonations']);}
+    if(isset($_POST['mtgWorship'])){$daMtg->setWorshipFac($_POST['mtgWorship']);}
+    if(isset($_POST['mtgAV'])){$daMtg->setAudioVisualFac($_POST['mtgAV']);}
+    if(isset($_POST['mtgSetup'])){$daMtg->setSetupFac($_POST['mtgSetup']);}
+    if(isset($_POST['mtgTransportation'])){$daMtg->setTransportationFac($_POST['mtgTransportation']);}
+    if(isset($_POST['mtgGreeter1'])){$daMtg->setGreeter1Fac($_POST['mtgGreeter1']);}
+    if(isset($_POST['mtgGreeter2'])){$daMtg->setGreeter2Fac($_POST['mtgGreeter2']);}
+    if(isset($_POST['mtgResources'])){$daMtg->setResourcesFac($_POST['mtgResources']);}
+    
+    if(isset($_POST['mtgMenu'])){$daMtg->setMenu($_POST['mtgMenu']);}
+    if(isset($_POST['mtgMealCnt'])){$daMtg->setMealCnt($_POST['mtgMealCnt']);}
+    if(isset($_POST['mtgMealFac'])){$daMtg->setMealFac($_POST['mtgMealFac']);}
+    
+    if(isset($_POST['mtgReader1'])){$daMtg->setReader1Fac($_POST['mtgReader1']);}
+    if(isset($_POST['mtgReader2'])){$daMtg->setReader2Fac($_POST['mtgReader2']);}
+    if(isset($_POST['mtgAnnouncements'])){$daMtg->setAnnouncementsFac($_POST['mtgAnnouncements']);}
+    if(isset($_POST['mtgTeaching'])){$daMtg->setTeachingFac($_POST['mtgTeaching']);}
+    if(isset($_POST['mtgChips1'])){$daMtg->setChips1Fac($_POST['mtgChips1']);}
+    if(isset($_POST['mtgChips2'])){$daMtg->setChips2Fac($_POST['mtgChips2']);}
+    if(isset($_POST['mtgNewcomers1'])){$daMtg->setNewcomers1Fac($_POST['mtgNewcomers1']);}
+    if(isset($_POST['mtgNewcomers2'])){$daMtg->setNewcomers2Fac($_POST['mtgNewcomers2']);}
+    if(isset($_POST['mtgSerenity'])){$daMtg->setSerenityFac($_POST['mtgSerenity']);}
+    
+    if(isset($_POST['mtgNursery'])){$daMtg->setNurseryCnt($_POST['mtgNursery']);}
+    if(isset($_POST['mtgNurseryFac'])){$daMtg->setNurseryFac($_POST['mtgNurseryFac']);}
+    if(isset($_POST['mtgChildren'])){$daMtg->setChildrenCnt($_POST['mtgChildren']);}
+    if(isset($_POST['mtgChildrenFac'])){$daMtg->setChildrenFac($_POST['mtgChildrenFac']);}
+    if(isset($_POST['mtgYouth'])){$daMtg->setYouthCnt($_POST['mtgYouth']);}
+    if(isset($_POST['mtgYouthFac'])){$daMtg->setYouthFac($_POST['mtgYouthFac']);}  
+    if(isset($_POST['mtgCafe'])){$daMtg->setCafeFac($_POST['mtgCafe']);}
+    if(isset($_POST['mtgTearDown'])){$daMtg->setTearDownFac($_POST['mtgTearDown']);}
+    if(isset($_POST['mtgSecurity'])){$daMtg->setSecurityFac($_POST['mtgSecurity']);}
 
-    
-    $mtgDate = $_POST['mtgDate'];
-    $mtgType = $_POST['rdoMtgType'];
-    $mtgTitle = $_POST['mtgTitle'];
-    
-    $mtgFac = $_POST['mtgCoordinator'];
-    $mtgAttendance = $_POST['mtgAttendance'];
-    $mtgDonations = $_POST['mtgDonations'];
-    $mtgWorshipFac = $_POST['mtgWorship'];
-    $mtgAudioVisualFac = $_POST['mtgAV'];
-    $mtgSetupFac = $_POST['mtgSetup'];
-    $mtgTransportationFac = $_POST['mtgTransportation'];
-    $mtgGreeter1Fac = $_POST['mtgGreeter1'];
-    $mtgGreeter2Fac = $_POST['mtgGreeter2'];
-    $mtgResourcesFac = $_POST['mtgResources'];
-    
-    $mtgMenu = $_POST['mtgMenu'];
-    $mtgMealCnt = $_POST['mtgMealCnt'];
-    $mtgMealFac = $_POST['mtgMealFac'];
-    
-    $mtgReader1Fac = $_POST['mtgReader1'];
-    $mtgReader2Fac = $_POST['mtgReader2'];
-    $mtgAnnouncementsFac = $_POST['mtgAnnouncements'];
-    $mtgTeachingFac = $_POST['mtgTeaching'];
-    $mtgChips1Fac = $_POST['mtgChips1'];
-    $mtgChips2Fac = $_POST['mtgChips2'];
-    $mtgNewcomers1Fac = $_POST['mtgNewcomers1'];
-    $mtgNewcomers2Fac = $_POST['mtgNewcomers2'];
-    $mtgSerenityFac = $_POST['mtgSerenity'];
-    
-    $mtgNurseryCnt = $_POST['mtgNursery'];
-    $mtgNurseryFac = $_POST['mtgNurseryFac'];
-    $mtgChildrenCnt = $_POST['mtgChildren'];
-    $mtgChildrenFac = $_POST['mtgChildrenFac'];
-    $mtgYouthCnt = $_POST['mtgYouth'];
-    $mtgYouthFac = $_POST['mtgYouthFac'];
-    
-    $mtgCafeFac = $_POST['mtgCafe'];
-    $mtgTearDownFac = $_POST['mtgTearDown'];
-    $mtgSecurityFac = $_POST['mtgSecurity'];
-    
-    $mtgNotes = $_POST['mtgNotes'];
-    
-    //         echo "\$mtgDate: " . $mtgDate . "<br/>";
-    //         echo "\$mtgType: $mtgType<br/>";
-    //         echo "\$mtgTitle: $mtgTitle<br/>";
-    //         echo "\$mtgFac: $mtgFac<br/>";
-    //         echo "\$mtgAttendance: $mtgAttendance<br/>";
-    //         echo "\$mtgDonations: $mtgDonations<br/>";
-    //         echo "\$mtgWorshipFac: $mtgWorshipFac<br/>";
-    //         echo "\$mtgAudioVisualFac: $mtgAudioVisualFac<br/>";
-    //         echo "\$mtgSetupFac: $mtgSetupFac<br/>";
-    //         echo "\$mtgTransportationFac: $mtgTransportationFac<br/>";
-    //         echo "\$mtgGreeter1Fac: $mtgGreeter1Fac<br/>";
-    //         echo "\$mtgGreeter2Fac: $mtgGreeter2Fac<br/>";
-    //         echo "\$mtgResourcesFac: $mtgResourcesFac<br/>";
-    //         echo "\$mtgMenu: $mtgMenu<br/>";
-    //         echo "\$mtgMealCnt: $mtgMealCnt<br/>";
-    //         echo "\$mtgMealFac: $mtgMealFac<br/>";
-    
-    //         echo "\$mtgReader1Fac: $mtgReader1Fac<br/>";
-    //         echo "\$mtgReader2Fac: $mtgReader2Fac<br/>";
-    //         echo "\$mtgAnnouncementsFac: $mtgAnnouncementsFac<br/>";
-    //         echo "\$mtgTeachingFac: $mtgTeachingFac<br/>";
-    
-    //         echo "\$mtgChips1Fac: $mtgChips1Fac<br/>";
-    //         echo "\$mtgChips2Fac: $mtgChips2Fac<br/>";
-    //         echo "\$mtgNewcomers1Fac: $mtgNewcomers1Fac<br/>";
-    //         echo "\$mtgNewcomers2Fac: $mtgNewcomers2Fac<br/>";
-    //         echo "\$mtgSerenityFac: $mtgSerenityFac<br/>";
-    //         echo "\$mtgNurseryCnt: $mtgNurseryCnt<br/>";
-    //         echo "\$mtgNurseryFac: $mtgNurseryFac<br/>";
-    //         echo "\$mtgChildrenCnt: $mtgChildrenCnt<br/>";
-    //         echo "\$mtgChildrenFac: $mtgChildrenFac<br/>";
-    //         echo "\$mtgYouthCnt: $mtgYouthCnt<br/>";
-    //         echo "\$mtgYouthFac: $mtgYouthFac<br/>";
-    //         echo "\$mtgCafeFac: $mtgCafeFac<br/>";
-    //         echo "\$mtgTearDownFac: $mtgTearDownFac<br/>";
-    //         echo "\$mtgSecurityFac: $mtgSecurityFac<br/>";
-    //         echo "\$mtgNotes: $mtgNotes<br/>";
-    //         exit();
+    if(isset($_POST['mtgNotes'])){$daMtg->setNotes($_POST['mtgNotes']); }
+    //meeting object should be loaded with values
+//     print_r($daMtg);
+//     exit();
     
     
     
@@ -152,122 +109,148 @@ function addMeetingToDB(){
     // mtgType
     // mtgTitle
     
+    $connection = mysqli_connect($_SESSION["MTR-H"], $_SESSION["MTR-U"], $_SESSION["MTR-P"], $_SESSION["MTR-N"]);
     
-    $connection = new mysqli(DB_HOST, DB_USER, DB_PASSWORD, DB_NAME);
+//     $connection = new mysqli(DB_HOST, DB_USER, DB_PASSWORD, DB_NAME);
     // Check connection
     if ($connection->connect_error) {
         die("Connection failed: " . $connection->connect_error);
     }
     $dbID = 0;
-    $stmt = $connection->prepare("Select ID, MtgDate, MtgType, MtgTitle FROM meetings WHERE MtgDate = ? AND MtgType = ? AND MtgTitle = ?");
-    $stmt->bind_param("sss", date("Y-m-d", strtotime($mtgDate)), $mtgType, mysql_real_escape_string($mtgTitle));
+    $stmt = $connection->prepare("Select ID, MtgDate, MtgType, MtgTitle FROM " . $_SESSION["MTR-CLIENT"] . ".meetings WHERE MtgDate = ? AND MtgType = ? AND MtgTitle = ?");
+    $dt = date("Y-m-d", strtotime($daMtg->mtgDate));
+    $tp = $daMtg->mtgType;
+    $tt = mysqli_real_escape_string($connection, $daMtg->mtgTitle);
+    $stmt->bind_param("sss", $dt, $tp, $tt);
     $stmt->execute();
     $stmt->store_result();
     if ($stmt->num_rows > 0){
         //echo "already entered";
-        $stmt->bind_result($ID, $dbDate, $dbType, $dbTitle);
+        $stmt->bind_result($ID, $MtgDate, $MtgType, $MtgTitle);
         while($stmt->fetch()){
-            $mtgID = $ID;
+            $daMtg->mtgID = $ID;
         }
         $stmt->free_result();
         $stmt->close();
         $connection->close();
-        destination(307, "error.php?ErrorCode=3001&ErrorMsg=\"That%20meeting%20already%20exists\"&ID=$mtgID");
+        destination(307, "error.php?ErrorCode=3001&ErrorMsg=\"That%20meeting%20already%20exists\"&ID=$daMtg->mtgID");
     }
     //--------------------------------------------------------------
     // no record found, proceed to add the entry
     //--------------------------------------------------------------
     $stmt->close();
     
-    //     $connection = new mysqli(DB_HOST, DB_USER, DB_PASSWORD, DB_NAME);
-    $stmt = $connection->prepare("INSERT INTO `meetings` ( `MtgDate`, `MtgType`, `MtgTitle`) VALUES ( ?, ?, ?)");
-    $stmt->bind_param("sss", date("Y-m-d", strtotime($mtgDate)), $mtgType, $mtgTitle );
-    $stmt->execute();
-    $stmt->close();
-    //--------------------------------------------------------------
-    // get the ID just created with the insert
-    //--------------------------------------------------------------
-    //     $stmt = $connection->prepare("Select ID FROM meetings WHERE MtgDate = ? AND MtgType = ? AND MtgTitle = ?");
-    //     $stmt->bind_param("sss", $mtgDate, $mtgType, $mtgTitle);
-    //     $stmt->execute();
-    //     $stmt->bind_result($ID);
-    $sql = "SELECT ID FROM meetings WHERE MtgDate = '";
-    $sql .= date("Y-m-d", strtotime($mtgDate)) . "' AND MtgType = '";
-    $sql .= $mtgType . "' AND  MtgTitle = '";
-    $sql .= mysql_real_escape_string($mtgTitle) . "'";
-    $result = $connection->query($sql, MYSQLI_STORE_RESULT);
-    list($returnedID) = $result->fetch_row();
-    $mtgID = $returnedID;
-    $result->close();
-    //----------------------------------------------------------
-    //now add (update) in sections
-    //----------------------------------------------------------
+//     echo "<br>Here is what we are going to save....<br>";
+//     print_r($daMtg);
+//     exit();
+    // API to insert meeting
+    $url = 'http://rogueintel.org/mapi/public/index.php/api/meeting/create/' . $_SESSION["MTR-CLIENT"];
     
-    //     $connection = new mysqli(DB_HOST, DB_USER, DB_PASSWORD, DB_NAME);
-    $sql = "UPDATE meetings SET MtgFac = ?, MtgAttendance = ?, Donations = ?, MtgWorship = ?, AudioVisualFac = ?, ";
-    $sql .= "SetupFac = ?, TransportationFac = ?, Greeter1Fac = ?, Greeter2Fac = ?, ResourcesFac = ? WHERE ID = ?";
-    $stmt = $connection->prepare($sql);
-    $stmt->bind_param("iidiiiiiiii",
-        $mtgFac,
-        $mtgAttendance,
-        $mtgDonations,
-        $mtgWorshipFac,
-        $mtgAudioVisualFac,
-        $mtgSetupFac,
-        $mtgTransportationFac,
-        $mtgGreeter1Fac,
-        $mtgGreeter2Fac,
-        $mtgResourcesFac,
-        $mtgID);
-    $stmt->execute();
-    $stmt->close();
     
-    $sql = "UPDATE meetings SET MealCnt = ?, MealFac = ?, Reader1Fac = ?, Reader2Fac = ?, AnnouncementsFac = ?, ";
-    $sql .= "TeachingFac = ?, Chips1Fac = ?, Chips2Fac = ?, SerenityFac = ? WHERE ID = ?";
-    $stmt = $connection->prepare($sql);
-    $stmt->bind_param("iiiiiiiiii",
-        $mtgMealCnt,
-        $mtgMealFac,
-        $mtgReader1Fac,
-        $mtgReader2Fac,
-        $mtgAnnouncementsFac,
-        $mtgTeachingFac,
-        $mtgChips1Fac,
-        $mtgChips2Fac,
-        $mtgSerenityFac,
-        $mtgID);
-    $stmt->execute();
-    $stmt->close();
+    $ch = curl_init($url);
+    //encode daMtr class
+    $jsonDataEncoded = json_encode($daMtg);
+    //Tell cURL that we want to send a POST reqeust
+    curl_setopt($ch, CURLOPT_POST, 1);
+    //Attach our encoded JSON string to the POST fields
+    curl_setopt($ch, CURLOPT_POSTFIELDS, $jsonDataEncoded);
+    // Set the content type to application/json
+    curl_setopt($ch, CURLOPT_HTTPHEADER, array('Content-Type: application/json'));
     
-    $sql = "UPDATE meetings SET Newcomers1Fac = ?, Newcomers2Fac = ?, NurseryCnt = ?, NurseryFac = ?, ";
-    $sql .= "ChildrenCnt = ?, ChildrenFac = ?, YouthCnt = ?, YouthFac = ? WHERE ID = ?";
-    $stmt = $connection->prepare($sql);
-    $stmt->bind_param("iiiiiiiii",
-        $mtgNewcomers1Fac,
-        $mtgNewcomers2Fac,
-        $mtgNurseryCnt,
-        $mtgNurseryFac,
-        $mtgChildrenCnt,
-        $mtgChildrenFac,
-        $mtgYouthCnt,
-        $mtgYouthFac,
-        $mtgID);
-    $stmt->execute();
-    $stmt->close();
+    //execute
+    $result = curl_exec($ch);
     
-    $sql = "UPDATE meetings SET CafeFac = ?, TearDownFac = ?, SecurityFac = ?, Menu = ?, ";
-    $sql .= "MtgNotes = ? WHERE ID = ?";
-    $stmt = $connection->prepare($sql);
-    $stmt->bind_param("iiissi",
-        $mtgCafeFac,
-        $mtgTearDownFac,
-        $mtgSecurityFac,
-        $mtgMenu,
-        $mtgNotes,
-        $mtgID);
-    $stmt->execute();
-    $stmt->close();
-    $connection->close();
+    
+    
+//     //     $connection = new mysqli(DB_HOST, DB_USER, DB_PASSWORD, DB_NAME);
+//     $stmt = $connection->prepare("INSERT INTO `meetings` ( `MtgDate`, `MtgType`, `MtgTitle`) VALUES ( ?, ?, ?)");
+//     $stmt->bind_param("sss", date("Y-m-d", strtotime($daMtg->mtgDate)), $daMtg->mtgType, $daMtg->mtgTitle );
+//     $stmt->execute();
+//     $stmt->close();
+//     //--------------------------------------------------------------
+//     // get the ID just created with the insert
+//     //--------------------------------------------------------------
+//     //     $stmt = $connection->prepare("Select ID FROM meetings WHERE MtgDate = ? AND MtgType = ? AND MtgTitle = ?");
+//     //     $stmt->bind_param("sss", $mtgDate, $mtgType, $mtgTitle);
+//     //     $stmt->execute();
+//     //     $stmt->bind_result($ID);
+//     $sql = "SELECT ID FROM meetings WHERE MtgDate = '";
+//     $sql .= date("Y-m-d", strtotime($daMtg->mtgDate)) . "' AND MtgType = '";
+//     $sql .= $daMtg->mtgType . "' AND  MtgTitle = '";
+//     $sql .= mysql_real_escape_string($daMtg->mtgTitle) . "'";
+//     $result = $connection->query($sql, MYSQLI_STORE_RESULT);
+//     list($returnedID) = $result->fetch_row();
+//     $daMtg->mtgID = $returnedID;
+//     $result->close();
+//     //----------------------------------------------------------
+//     //now add (update) in sections
+//     //----------------------------------------------------------
+    
+//     //     $connection = new mysqli(DB_HOST, DB_USER, DB_PASSWORD, DB_NAME);
+//     $sql = "UPDATE meetings SET MtgFac = ?, MtgAttendance = ?, Donations = ?, MtgWorship = ?, AudioVisualFac = ?, ";
+//     $sql .= "SetupFac = ?, TransportationFac = ?, Greeter1Fac = ?, Greeter2Fac = ?, ResourcesFac = ? WHERE ID = ?";
+//     $stmt = $connection->prepare($sql);
+//     $stmt->bind_param("iidiiiiiiii",
+//         $daMtg->mtgFac,
+//         $daMtg->mtgAttendance,
+//         $daMtg->mtgDonations,
+//         $daMtg->mtgWorshipFac,
+//         $daMtg->mtgAudioVisualFac,
+//         $daMtg->mtgSetupFac,
+//         $daMtg->mtgTransportationFac,
+//         $daMtg->mtgGreeter1Fac,
+//         $daMtg->mtgGreeter2Fac,
+//         $daMtg->mtgResourcesFac,
+//         $daMtg->mtgID);
+//     $stmt->execute();
+//     $stmt->close();
+    
+//     $sql = "UPDATE meetings SET MealCnt = ?, MealFac = ?, Reader1Fac = ?, Reader2Fac = ?, AnnouncementsFac = ?, ";
+//     $sql .= "TeachingFac = ?, Chips1Fac = ?, Chips2Fac = ?, SerenityFac = ? WHERE ID = ?";
+//     $stmt = $connection->prepare($sql);
+//     $stmt->bind_param("iiiiiiiiii",
+//         $daMtg->mtgMealCnt,
+//         $daMtg->mtgMealFac,
+//         $daMtg->mtgReader1Fac,
+//         $daMtg->mtgReader2Fac,
+//         $daMtg->mtgAnnouncementsFac,
+//         $daMtg->mtgTeachingFac,
+//         $daMtg->mtgChips1Fac,
+//         $daMtg->mtgChips2Fac,
+//         $daMtg->mtgSerenityFac,
+//         $daMtg->mtgID);
+//     $stmt->execute();
+//     $stmt->close();
+    
+//     $sql = "UPDATE meetings SET Newcomers1Fac = ?, Newcomers2Fac = ?, NurseryCnt = ?, NurseryFac = ?, ";
+//     $sql .= "ChildrenCnt = ?, ChildrenFac = ?, YouthCnt = ?, YouthFac = ? WHERE ID = ?";
+//     $stmt = $connection->prepare($sql);
+//     $stmt->bind_param("iiiiiiiii",
+//         $daMtg->mtgNewcomers1Fac,
+//         $daMtg->mtgNewcomers2Fac,
+//         $daMtg->mtgNurseryCnt,
+//         $daMtg->mtgNurseryFac,
+//         $daMtg->mtgChildrenCnt,
+//         $daMtg->mtgChildrenFac,
+//         $daMtg->mtgYouthCnt,
+//         $daMtg->mtgYouthFac,
+//         $daMtg->mtgID);
+//     $stmt->execute();
+//     $stmt->close();
+    
+//     $sql = "UPDATE meetings SET CafeFac = ?, TearDownFac = ?, SecurityFac = ?, Menu = ?, ";
+//     $sql .= "MtgNotes = ? WHERE ID = ?";
+//     $stmt = $connection->prepare($sql);
+//     $stmt->bind_param("iiissi",
+//         $daMtg->mtgCafeFac,
+//         $daMtg->mtgTearDownFac,
+//         $daMtg->mtgSecurityFac,
+//         $daMtg->mtgMenu,
+//         $daMtg->mtgNotes,
+//         $daMtg->mtgID);
+//     $stmt->execute();
+//     $stmt->close();
+//     $connection->close();
     
     destination(307, "meetings.php");
     
